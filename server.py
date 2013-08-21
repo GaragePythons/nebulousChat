1041fde1131c377c55d42ef5a5c81fec8c989c3c
Garage Pythons <garagepythons@gmail.com>
2013-08-21 22:52:49 +0100

    re-jigged server-side query-handling code

--- server.py
+++ server.py
@@ -12,18 +12,33 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
     """
 
     def handle(self):
-        # self.request is the TCP socket connected to the client.
-        while True:
+
+        class ClientQuit(Exception):
+            pass
+
+        # There should be a way to move this definition outside of handle()...
+        def getQuery():
             self.data = self.request.recv(1024)
             if self.data == '':
                 print "[Client quit.]"
-                break
+                raise ClientQuit
             self.request.sendall(self.data) # Send back the same data.
+            return unserialize(self.data)
 
-            self.query = unserialize(self.data)
+        # self.request is the TCP socket connected to the client.
+        while True:
+            try:
+                self.query = getQuery()
+            # Is this a good way to be using exceptions...?
+            except ClientQuit:
+                break
+
             # Work out what the client wants...
+            print self.query.__class__.__name__
 
-            # self.server.messages.append((self.client_address, self.data))
+            # This is broken.
+            self.server.globalQueriesList.append(self.query)
+            print self.server.globalQueriesList.append(self.query)
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
@@ -34,7 +49,7 @@ if __name__ == "__main__":
 
     # Threading voodoo
     server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
-    server.messages = []
+    server.globalQueriesList = []
     server_thread = threading.Thread(target=server.serve_forever)
     server_thread.daemon = True
 
