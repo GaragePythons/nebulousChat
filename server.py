c730148e795efe7cc4f549734491da022909fd29
Sam Nipps <sam.nipps+github@gmail.com>
2013-08-25 22:13:40 +0100

    Unfucked Nic's shit

--- server.py
+++ server.py
@@ -5,48 +5,28 @@ import thread
 import SocketServer
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
-
     def handle(self):
-        
-        class ClientQuit(Exception):
-            pass
-
         def getConnectionType():
             pass
-
-        # There should be a way to move this definition outside of handle()...
-        def getQueries():
-            while True: 
-                self.data = self.request.recv(1024)
-                if self.data == '':
-                    print "[Client quit.]"
-                    raise ClientQuit
-                self.request.sendall(self.data) # Send back the same data.
-                return unserialize(self.data)
-
-        def sendQueries():
-            while True:
-                self.server.queryQueue.get()
-
-
+    
+        queries = self.server.queries
+    
         print(  "[" + self.client_address[0] + ":"
               + str(self.client_address[1]) + " connected.]")
 
-        # self.request is the TCP socket connected to the client.
         while True:
-            # Wait for EITHER client query OR news from another handle().
-            try:
-                self.query = getQuery()
-            # Is this a good way to be using exceptions...?
-            except ClientQuit:
+            # self.request is the TCP socket connected to the client.
+            data = self.request.recv(1024)
+            self.request.sendall(data)
+            
+            if data == "":
                 break
 
-            # Work out what the client wants...
-            print self.query.__class__.__name__
-
-            # This is broken.
-            self.server.queryQueue.put(self.query)
-            # print self.server.globalQueriesList.append(self.query)
+            query = unserialize(data)
+            print query.__class__.__name__
+            queries.put(query)
+            
+        print "[Client quit.]"
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
@@ -54,10 +34,9 @@ class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9998
 
-
     # Threading voodoo
     server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
-    server.queryQueue = Queue.Queue()
+    server.queries = Queue.Queue()
     # server.globalQueriesList = []
     serverThread = threading.Thread(target=server.serve_forever)
     serverThread.daemon = True
