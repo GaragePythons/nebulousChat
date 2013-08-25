44872979f852aae168e7610a067af7f68855b746
Garage Pythons <garagepythons@gmail.com>
2013-08-25 21:53:05 +0100

    half-way edit of server.py

--- server.py
+++ server.py
@@ -1,32 +1,40 @@
 from serializing import serialize, unserialize
+import Queue
 import threading
+import thread
 import SocketServer
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
-    """
-    The RequestHandler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
 
     def handle(self):
-
+        
         class ClientQuit(Exception):
             pass
 
+        def getConnectionType():
+            pass
+
         # There should be a way to move this definition outside of handle()...
-        def getQuery():
-            self.data = self.request.recv(1024)
-            if self.data == '':
-                print "[Client quit.]"
-                raise ClientQuit
-            self.request.sendall(self.data) # Send back the same data.
-            return unserialize(self.data)
+        def getQueries():
+            while True: 
+                self.data = self.request.recv(1024)
+                if self.data == '':
+                    print "[Client quit.]"
+                    raise ClientQuit
+                self.request.sendall(self.data) # Send back the same data.
+                return unserialize(self.data)
+
+        def sendQueries():
+            while True:
+                self.server.queryQueue.get()
+
+
+        print(  "[" + self.client_address[0] + ":"
+              + str(self.client_address[1]) + " connected.]")
 
         # self.request is the TCP socket connected to the client.
         while True:
+            # Wait for EITHER client query OR news from another handle().
             try:
                 self.query = getQuery()
             # Is this a good way to be using exceptions...?
@@ -37,20 +45,21 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
             print self.query.__class__.__name__
 
             # This is broken.
-            self.server.globalQueriesList.append(self.query)
-            print self.server.globalQueriesList.append(self.query)
+            self.server.queryQueue.put(self.query)
+            # print self.server.globalQueriesList.append(self.query)
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
+    HOST, PORT = "localhost", 9998
 
 
     # Threading voodoo
     server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
-    server.globalQueriesList = []
-    server_thread = threading.Thread(target=server.serve_forever)
-    server_thread.daemon = True
+    server.queryQueue = Queue.Queue()
+    # server.globalQueriesList = []
+    serverThread = threading.Thread(target=server.serve_forever)
+    serverThread.daemon = True
 
     server.serve_forever()
